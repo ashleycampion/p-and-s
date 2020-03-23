f2a32914537f9795e3127574d32efc4a3b8fb776
ashleycampion <ashleydcampion@gmail.com>
2020-03-23 22:32:38 +0000

    standardizing file name

--- Week5-Weekday.py
+++ Week5-Weekday.py
@@ -11,7 +11,6 @@ x = datetime.datetime.now().weekday()
 
 # if weekday value 'x' is less than 5 then it is a weekday,
 # otherwise it is the weekend
-
 if x < 5:
     print("I'm afraid today's a weekday, up you get now...")
 else:
