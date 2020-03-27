5bcf88c7cb720b4fda4d63c017793f2b557c0d05
ashleycampion <ashleydcampion@gmail.com>
2020-03-27 19:36:03 +0000

    realised I had duplicated the code for this, so deleting the duplicate

--- Week2-BMI.py
+++ Week2-BMI.py
@@ -1,8 +1,3 @@
-height = float(input("Enter height in meters:"))
-weight = float(input("Enter weight in kgs:"))
-bmi = weight / height**2
-print("Your BMI is {:.2F}".format(bmi))
-
 # Programme for calculating BMI
 # BMI formula is: weight-in-kgs / height-in-metres ** 2
 
